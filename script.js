08469ce10703451ce7147446955513b6a99b2b42
Maxwell <166428177+Maxwellspring@users.noreply.github.com>
2025-03-14 10:44:16 -0700

    YEAAAAAA

--- script.js
+++ script.js
@@ -1,23 +1,48 @@
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
 import * as THREE from 'three';
+
+const loader = new GLTFLoader();
+
+loader.load( 'model/model.gltf', function ( gltf ) {
+
+	scene.add( gltf.scene );
+
+}, undefined, function ( error ) {
+
+	console.error( error );
+
+} );
 
 const width = window.innerWidth, height = window.innerHeight;
 
 // init
 
 const camera = new THREE.PerspectiveCamera( 60, width / height, 0.01, 100 );
-camera.position.z = 1;
-camera.position.x = 0.1;
-camera.position.y = 0.2;
+camera.position.z = 1.5;
+camera.position.x = 0.4;
+camera.position.y = 0.6;
+camera.lookAt(0,0,0)
 
 const scene = new THREE.Scene();
 
 const geometry = new THREE.BoxGeometry( 0.1, 0.4, 0.2 );
-const material = new THREE.MeshNormalMaterial();
+const floorGeometry = new THREE.BoxGeometry( 1, 0.01, 1 );
+const material = new THREE.LineBasicMaterial( { color: 0x964B00 } );
+const boxMaterial = new THREE.LineBasicMaterial( { color: 0x0000ff } );
 
-const mesh = new THREE.Mesh( geometry, material );
+const backMaterial = new THREE.LineBasicMaterial( { color: 0xFFFFFF } );
+const backGeometry = new THREE.BoxGeometry( 2, 2, 0.01 );
+
+const mesh = new THREE.Mesh( geometry, boxMaterial );
+const myMesh = new THREE.Mesh( floorGeometry, material );
+const back = new THREE.Mesh( backGeometry, backMaterial )
 scene.add( mesh );
+scene.add( myMesh );
+scene.add( back );
 
-const renderer = new THREE.WebGLRenderer( { antialias: false } );
+
+const renderer = new THREE.WebGLRenderer( { antialias: true } );
 renderer.setSize( width, height, );
 renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
@@ -29,6 +54,8 @@ function animate( time ) {
 	mesh.rotation.x = time / 1000;
 	mesh.rotation.y = time / 100;
   mesh.rotation.z = time / 1000;
+  myMesh.position.y = -0.4
+  back.position.z = -0.4
 
 	renderer.render( scene, camera );
 
