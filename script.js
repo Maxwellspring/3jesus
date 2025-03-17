b26456ba9007e8f492cbda448078d1339e966ac8
Maxwell <166428177+Maxwellspring@users.noreply.github.com>
2025-03-17 09:48:09 -0700

    heck yea camera controls

--- script.js
+++ script.js
@@ -6,60 +6,78 @@ import * as THREE from 'three';
 
 const loader = new GLTFLoader();
 
-loader.load( 'model/model.gltf', function ( gltf ) {
+function modelLoader(modelName) {
+	loader.load(`model/${modelName}.gltf`, function (gltf) {
+		scene.add(gltf.scene);
+	}, undefined, function (error) {
+		console.error(error);
+	});
+};
 
-	scene.add( gltf.scene );
 
-}, undefined, function ( error ) {
 
-	console.error( error );
-
-} );
+modelLoader("model");
+modelLoader("TT");
 
 const width = window.innerWidth, height = window.innerHeight;
 
 // init
 
-const camera = new THREE.PerspectiveCamera( 60, width / height, 0.01, 100 );
-camera.position.z = 1.5;
-camera.position.x = CHANGE;
-camera.position.y = 0.6;
-camera.lookAt(0,0,0)
+let number = 3
+
+const camera = new THREE.PerspectiveCamera(60, width / height, 0.01, 100);
+camera.position.z = 3;
+camera.position.x = 3;
+
+document.addEventListener('keyup', function(event) {
+	console.log('Key up:', event.key, 'Code:', event.code);
+	number = parseInt(event.key)
+	console.log(number) 
+	camera.position.z = number;
+camera.position.x = number;
+	camera.position.y = number;
+	return number
+  });
+
+
+
+camera.lookAt(0, 0, 0)
 
 const scene = new THREE.Scene();
 
-const geometry = new THREE.BoxGeometry( 0.1, 0.4, 0.2 );
-const floorGeometry = new THREE.BoxGeometry( 1, 0.01, 1 );
-const material = new THREE.LineBasicMaterial( { color: 0x964B00 } );
-const boxMaterial = new THREE.LineBasicMaterial( { color: 0x0000ff } );
+const geometry = new THREE.BoxGeometry(0.1, 0.4, 0.2);
+const floorGeometry = new THREE.BoxGeometry(1, 0.01, 1);
+const material = new THREE.LineBasicMaterial({ color: 0x964B00 });
+const boxMaterial = new THREE.LineBasicMaterial({ color: 0x0000ff });
 
-const backMaterial = new THREE.LineBasicMaterial( { color: 0xFFFFFF } );
-const backGeometry = new THREE.BoxGeometry( 2, 2, 0.01 );
+const backMaterial = new THREE.LineBasicMaterial({ color: 0xFFFFFF });
+const backGeometry = new THREE.BoxGeometry(10, 10, 0.01);
 
-const mesh = new THREE.Mesh( geometry, boxMaterial );
-const myMesh = new THREE.Mesh( floorGeometry, material );
-const back = new THREE.Mesh( backGeometry, backMaterial )
-scene.add( mesh );
-scene.add( myMesh );
-scene.add( back );
+const mesh = new THREE.Mesh(geometry, boxMaterial);
+const myMesh = new THREE.Mesh(floorGeometry, material);
+const back = new THREE.Mesh(backGeometry, backMaterial)
+scene.add(mesh);
+scene.add(myMesh);
+scene.add(back);
 
 
-const renderer = new THREE.WebGLRenderer( { antialias: true } );
-renderer.setSize( width, height, );
-renderer.setAnimationLoop( animate );
-document.body.appendChild( renderer.domElement );
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(width, height,);
+renderer.setAnimationLoop(animate);
+document.body.appendChild(renderer.domElement);
 
 // animation
 
-function animate( time ) {
+function animate(time) {
 
 	mesh.rotation.x = time / 1000;
 	mesh.rotation.y = time / 100;
-  mesh.rotation.z = time / 1000;
-  myMesh.position.y = -0.4
-  back.position.z = -0.4
+	mesh.rotation.z = time / 1000;
+	myMesh.position.y = -0.4
+	back.position.z = -1.2
+	//   gltf.scene.position.x = 1
 
-	renderer.render( scene, camera );
+	renderer.render(scene, camera);
 
 }
 
--- script2.js
+++
@@ -1,9 +0,0 @@
-// const button = document.getElementById("i")
-// const thing = document.getElementById("p")
-// let value = 0
-
-// button.addEventListener("click", function(){
-//     value++
-//     thing.textContent = value
-//     return value
-// }) 
